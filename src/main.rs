b38e93bb72ae11271325abe1ca9c7e1a24774a2e
Alex <alexanderthestudent@yandex.ru>
2025-08-03 19:15:08 +0300

    feat: layout & basic doc toolbar

--- src/main.rs
+++ src/main.rs
@@ -9,74 +9,98 @@ fn main() {
         .expect("Can't get document");
     let body = document.body().expect("Can't get body");
 
-    let text = document.create_element("div").unwrap();
-    text.set_text_content(Some("Hello, world."));
-    body.append_child(&text).unwrap();
+    let root = append_new_div(&document, &body, "root");
+    let graph_container = append_new_div(&document, &root, "graph");
+    let page_toolbar = append_new_div(&document, &root, "toolbar toolbar--page");
+    let doc_toolbar = append_new_div(&document, &root, "toolbar toolbar--doc");
+    let page_container = append_new_div(&document, &root, "page");
 
-    let buttons = vec![Button::new(
-        Icon::Next,
-        Shortcut {
-            mode: vec![Mode::Normal],
-            ctrl: false,
-            key: Key::Down,
-        },
-    )];
+    let button_help = append_new_button(&document, &doc_toolbar, Action::Help);
+    let _ = append_new_div(&document, &doc_toolbar, "toolbar__spacer");
+    let button_prev = append_new_button(&document, &doc_toolbar, Action::PrevPage);
+    let button_next = append_new_button(&document, &doc_toolbar, Action::NextPage);
 }
 
-struct Button {
-    pub icon: Icon,
-    pub shortcut: Shortcut,
-    pub rx: Receiver<()>,
-    tx: Sender<()>,
+/// - Creates a new `div` element
+/// - Appends it to `parent`
+/// - Assigns it the `class`
+/// - Gives you the ownership
+///
+/// ## Panics
+/// - if `document.create_element` fails
+/// - if `parent.append_child` fails
+fn append_new_div(document: &Document, parent: &Element, class: &str) -> Element {
+    let div = document
+        .create_element("div")
+        .expect("Can't create a DOM element");
+    div.set_class_name(class);
+    parent
+        .append_child(&div)
+        .expect("Can't append a DOM element");
+    div
 }
 
-struct Shortcut {
-    mode: Vec<Mode>,
-    ctrl: bool,
-    key: Key,
-}
-
-enum Key {
-    Char(char),
-    Esc,
-    Left,
-    Down,
-    Up,
-    Right,
-    Return,
-}
-
-enum Mode {
-    Readonly,
-    Normal,
-    Visual,
-    VisualLine,
-    VisualBlock,
-    Insert,
-    Object,
-}
-
+#[derive(Clone, Copy)]
 enum Icon {
+    QuestionMark,
     Next,
     Prev,
-    QuestionMark,
 }
 
-impl Button {
-    fn new(icon: Icon, shortcut: Shortcut) -> Self {
-        let (tx, rx) = channel();
+#[derive(Clone, Copy)]
+enum Action {
+    NextPage,
+    PrevPage,
+    Help,
+}
 
-        Self {
-            icon,
-            shortcut,
-            rx,
-            tx,
+impl Icon {
+    fn to_svg(self) -> &'static str {
+        match self {
+            Self::QuestionMark => include_str!("../icons/circle-question-mark.svg"),
+            Self::Next => include_str!("../icons/chevron-right.svg"),
+            Self::Prev => include_str!("../icons/chevron-left.svg"),
         }
     }
 }
 
-fn build_toolbar(document: &Document, buttons: &Vec<Button>) -> Option<Element> {
-    let container = document.create_element("div").ok()?;
+impl Action {
+    fn to_icon(self) -> Icon {
+        match self {
+            Self::NextPage => Icon::Next,
+            Self::PrevPage => Icon::Prev,
+            Self::Help => Icon::QuestionMark,
+        }
+    }
 
-    None
+    fn to_hotkey(self) -> char {
+        match self {
+            Self::NextPage => 'n',
+            Self::PrevPage => 'p',
+            Self::Help => 'h',
+        }
+    }
+}
+
+fn append_new_button(document: &Document, parent: &Element, action: Action) -> Element {
+    let button = document
+        .create_element("button")
+        .expect("Can't create a button in the DOM");
+    button.set_class_name("button");
+    button.set_inner_html(action.to_icon().to_svg());
+
+    let hotkey = action.to_hotkey().to_string();
+    let hotkey_element = document
+        .create_element("span")
+        .expect("Can't create a span in the DOM");
+    hotkey_element.set_class_name("button__hotkey");
+    hotkey_element.set_text_content(Some(&hotkey));
+    button
+        .append_child(&hotkey_element)
+        .expect("Can't append a DOM element");
+
+    parent
+        .append_child(&button)
+        .expect("Can't append a DOM element");
+    button
 }
